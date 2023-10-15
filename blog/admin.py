4c0892edb52e7471e9d21e88976f9b9278870bec
Danish <danishelahi016@gmail.com>
2023-10-15 13:41:33 +0500

    form created

--- blog/admin.py
+++ blog/admin.py
@@ -1,25 +1,20 @@
 from django.contrib import admin
-from .models import BlogPost , Category , Tag
+from .models import BlogPost , Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
     
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title' , 'author' , 'created_at' , 'updated_at')
-    list_filter = ('author' , 'categories' , 'tags')
+    list_filter = ('author' , 'categories')
     search_fields = ('title' , 'content')
-    # prepopulated_fields = {'slug':('title',)}
     date_hierarchy = 'created_at'
     ordering = ('-created_at',)
     
     fieldsets = (
         ('Post Info' , {
-            'fields':('title' , 'author' , 'categories', 'tags' , 'content')
+            'fields':('title' , 'author' , 'categories' , 'content')
         }),
     )
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path , include
+from django.urls import path , reverse_lazy
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('blog_list/' , views.PostList.as_view(), name = 'blog_list'),
-    path('blog_detail/<int:pk_from_url>' , views.PostDetail.as_view(), name = 'blog_detail'),
+    path('' , views.PostList.as_view(), name = 'blog_list'),
+    path('<int:pk_from_url>/detail' , views.PostDetail.as_view(), name = 'blog_detail'),
+    path('blog_create/' ,
+        views.PostCreate.as_view(success_url = reverse_lazy('blog:blog_list')), name = 'blog_create'),
+    path('cat_create/' ,
+        views.CatCreate.as_view(success_url = reverse_lazy('blog:blog_create')), name = 'cat_create'),
+    path('<int:pk>/delete', 
+        views.PostDelete.as_view(success_url=reverse_lazy('blog:blog_list')), name='blog_delete'),
+    path('<int:pk>/update', 
+        views.PostUpdate.as_view(success_url=reverse_lazy('blog:blog_list')), name='blog_Update'),
 ]
