a53215e18e784b05c14003c90fbf6b06f27d3789
Danish Elahi <90791396+Danish188@users.noreply.github.com>
2023-10-15 14:47:04 +0500

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,41 +1,23 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import BlogPost , Category , Tag
-=======
-from .models import BlogPost , Category
->>>>>>> post-creation
+from .models import BlogPost, Category
+from .models import BlogPost, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
-<<<<<<< HEAD
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-=======
->>>>>>> post-creation
-    
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title' , 'author' , 'created_at' , 'updated_at')
-<<<<<<< HEAD
-    list_filter = ('author' , 'categories' , 'tags')
-    search_fields = ('title' , 'content')
-    # prepopulated_fields = {'slug':('title',)}
-=======
-    list_filter = ('author' , 'categories')
-    search_fields = ('title' , 'content')
->>>>>>> post-creation
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    list_filter = ('author', 'categories')
+    search_fields = ('title', 'content')
+    list_filter = ('author', 'categories')
+    search_fields = ('title', 'content')
     date_hierarchy = 'created_at'
     ordering = ('-created_at',)
     
     fieldsets = (
-        ('Post Info' , {
-<<<<<<< HEAD
-            'fields':('title' , 'author' , 'categories', 'tags' , 'content')
-=======
-            'fields':('title' , 'author' , 'categories' , 'content')
->>>>>>> post-creation
+        ('Post Info', {
+            'fields':('title', 'author', 'categories', 'content')
         }),
     )
